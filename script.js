c4e53baad45bd2a8578030c2c88a2d107f0e12e7
Prity <prity12148@gmail.com>
2023-07-01 14:00:09 +0530

    Grocery

---
+++ script.js
@@ -0,0 +1,98 @@
+let searchForm =document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchForm.classList.toggle('active');
+    // searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart =document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () =>
+{
+    // shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm =document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = () =>
+{
+    // loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar =document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>
+{
+    // navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () =>
+{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    // slidesPerView: 1,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    }
+   ,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        // spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        // spaceBetween: 40,
+      },
+      1020: {
+        slidesPerView: 3,
+        // spaceBetween: 50,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    // slidesPerView: 1,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    }
+   ,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        // spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        // spaceBetween: 40,
+      },
+      1020: {
+        slidesPerView: 3,
+        // spaceBetween: 50,
+      },
+    },
+  });
